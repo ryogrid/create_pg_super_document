3a87c025b0f6ab731baa42912399dcb7655b017a
Ryo Kanbayashi <ryo.contact@gmail.com>
2025-09-21 12:09:43 +0900

    Clean up comment in convert_html_to_markdown function
    Remove unnecessary comment on body_width setting.

--- official_doc_in_md/html2md.py
+++ official_doc_in_md/html2md.py
@@ -6,7 +6,7 @@ def convert_html_to_markdown(site_dir):
     h = html2text.HTML2Text()
     h.ignore_links = False
     h.ignore_images = False
-    h.body_width = 0  # 行の折り返しを無効
+    h.body_width = 0
     
     for html_file in Path(site_dir).rglob('*.html'):
         with open(html_file, 'r', encoding='utf-8') as f:
